79eab6d407ee3a2d70169b07fa486ef88bc7ef06
DESKTOP-ONTRMRV\lsy01 <lsy01085751@gmail.com>
2022-02-26 11:50:46 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
+const customers=[
+  {
+  'id' :1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'홍동',
+  'birthday' : '000108'
+  }, {
+    'id' :2,
+    'image': 'https://placeimg.com/64/64/any',
+    'name':'홍동',
+    'birthday' : '000108'
+    },
+    {
+      'id' :3,
+      'image': 'https://placeimg.com/64/64/any',
+      'name':'홍동',
+      'birthday' : '000108'
+      }
+]
+function App() {  
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>ddd</h2>
-    </div>
+    <div>{
+    customers.map(c => {
+      return (
+        <Customer
+        key={c.id}
+         id ={c.id}
+         name={c.name}
+         image={c.image}
+         birthday={c.birthday}
+         />
+      )
+    })
+  }</div>
   );
 }
+
 
 export default App;
